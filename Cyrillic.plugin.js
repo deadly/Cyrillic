624330bd33eb0ef3d1dd784f3f3b891dad564187
Seraph <73450703+deadly@users.noreply.github.com>
2020-12-08 05:20:53 -0500

    Update Cyrillic.plugin.js

--- Cyrillic.plugin.js
+++ Cyrillic.plugin.js
@@ -4,7 +4,7 @@
  * @authorId 686440496561913856
  * @website https://sendthigh.pics
  * @description Used for replacing suitable characters in a message with Cyrillic characters to bypass blacklisted word filters or to evade keyword detection
- * @version 1.1.0
+ * @version 1.3.0
  * @updateUrl https://raw.githubusercontent.com/deadly/Cyrillic/main/Cyrillic.plugin.js
  */
 
