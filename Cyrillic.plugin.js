089cb4a435e3144281464d7d750b38a7ffc8c2d7
Seraph <73450703+deadly@users.noreply.github.com>
2020-12-09 11:30:31 -0500

    Added -ignore command
    If you prefix a message with -ignore, the plugin will not replace characters in that message

--- Cyrillic.plugin.js
+++ Cyrillic.plugin.js
@@ -4,7 +4,7 @@
  * @authorId 686440496561913856
  * @website https://sendthigh.pics
  * @description Used for replacing suitable characters in a message with Cyrillic characters to bypass blacklisted word filters or to evade keyword detection
- * @version 1.7.5
+ * @version 1.8.5
  * @source https://raw.githubusercontent.com/deadly/Cyrillic/main/Cyrillic.plugin.js
  * @updateUrl https://raw.githubusercontent.com/deadly/Cyrillic/main/Cyrillic.plugin.js
  */
@@ -23,7 +23,7 @@ module.exports =(() => {
 					github_username: "deadly",
 				}
 			],
-			version: "1.7.5",
+			version: "1.8.5",
 			description: "Used for replacing suitable characters in a message with Cyrillic characters to bypass blacklisted word filters or to evade keyword detection"
         }
     };  
@@ -111,8 +111,12 @@ module.exports =(() => {
                 }
     
                 async getCyrillic(textValue) {
+                    lowered = textValue.toLowerCase();
+                    if (lowered.includes('-ignore')) {
+                        textValue = textValue.replace('-ignore', '')
+                        return textValue;
+                    }
                     return textValue.split(' ').map(word => {
-			            lowered = word.toLowerCase();
                         if (
                             !lowered.includes('discord.gg')
                             && !lowered.includes('http')
